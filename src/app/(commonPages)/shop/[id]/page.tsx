3aca62db927ae14cfe723af8bde1af95b04a4d55
dev-sajjadhosan <devsajjadhosan@gmail.com>
2026-01-29 23:17:36 +0600

    feat: implement shop details page || implement overview and review tab components

--- src/app/(commonPages)/shop/[id]/page.tsx
+++ src/app/(commonPages)/shop/[id]/page.tsx
@@ -22,11 +22,15 @@ import { Button } from "@/components/ui/button";
 import { TooltipButton } from "@/components/ui/tooltip-button";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { useState } from "react";
+import ReviewComponent from "@/components/modules/shopPage/review";
+import OverviewComponent from "@/components/modules/shopPage/overview";
 
 export default function ShopDetails() {
   const price = 20.8;
   const [totalMoney, setTotalMoney] = useState<number>(price);
   const [quantity, setQuantity] = useState<number>(1);
+  const [tab, setTab] = useState("overview");
+
   return (
     <>
       <div className="w-11/12 mx-auto flex flex-col gap-24">
@@ -135,97 +139,21 @@ export default function ShopDetails() {
         </section>
         <section className="">
           <div className="border rounded-full px-5 py-3 flex items-center justify-center gap-3">
-            <Button variant={"default"}>
-              <BriefcaseMedical /> Description
+            <Button
+              variant={tab === "overview" ? "default" : "secondary"}
+              onClick={() => setTab("overview")}
+            >
+              <BriefcaseMedical /> Overview
             </Button>
-            <Button variant={"secondary"}>
+            <Button
+              variant={tab === "review" ? "default" : "secondary"}
+              onClick={() => setTab("review")}
+            >
               <UserStar />
               Review
             </Button>
           </div>
-          <p className="text-md tracking-wider mt-5 text-muted-foreground">
-            Indications of Adovas 200 ml This Adovas liquefies phlegm. It
-            soothes irritation of the throat. Helps to relieve hoarseness. It is
-            a remedy for all types of cough such as dry irritable cough,
-            allergic & smokers cough.
-            <br />
-            <br />
-            Composition: Each teaspoon full (5 ml) of syrup contains extracts
-            of- Adhatoda vasica 0.68 gm Piper longum 0.14 gm Glycyrrhiza glabra
-            6.78 mg Trikatu (Piper nigrum, Zingiber officinale) 20.34 mg
-            Terminalia chebula 73.24 mg Vitis venifera 0.14 gm Acorus calamus
-            6.78 mg Saussurea lappa 6.78 mg Syzygium aromaticum 6.78 mg Trizatak
-            (Cinnamomum zeylanicum, Elettaria cardamomum, Cinnamomum tamala)
-            20.34 mg Pistacia integerrima 6.78 mg Myrica nagi 6.78 mg Woodfordia
-            fruticosa 1.14 mg.
-            <br />
-            <br />
-            Dosage of Adovas 200 ml: Children under 12 years: 1-2 teaspoonful
-            (5-10 ml) 2-3 times a day. 'Adult: 3 teaspoonful (15 ml) 2-3 times a
-            day.Some warm water may be added for better results.
-            <br />
-            <br />
-            Interaction of Adovas 200 ml: No report is available.
-            <br />
-            <br />
-            Contraindications: No report is available on contraindication. It
-            may be happen in patients who are hypersensitive to any of its
-            ingredients.
-            <br />
-            <br />
-            Side Effects of Adovas 200 ml: This syrup is proven as safe. It is
-            well tolerated. In high dose diarrhea, vomiting may occur.
-            <br />
-            <br />
-            Storage Conditions: Keep out of reach of children. Keep away from
-            direct sunlight. Store in a cool and dry place.
-            <br />
-            <br />
-            Drug Classes: Herbal and Nutraceuticals
-            <br />
-            <br />
-            Mode Of Action: Adhatoda vasica (Basok): Relieves cough & bronchial
-            spasm. It liquefies mucous. Piper longum (Pipul): Relieves cold
-            allergy & asthma. Glycyrrhiza glabra (Jashthi Modhu) : Relieves
-            irritation of throat. Enhances the immune system. It is
-            anti-inflammatory, demulcent & expectorant. Piper nigrum (Marich):
-            It is fungistatic, bacteriostatic & anti-inflammatory. Zingiber
-            officinale (Shunthi): It is antihistaminic. Very much effective in
-            common cold. Terminalia chebula (Haritaki): It removes toxin from
-            the body. It has beneficial effect on all tissues. Vitis venifera
-            (Kismiss): Relieves cough and general tonic. Acorus calamus (Bacha):
-            Helps in bronchial catarrh & intermittent fever. Saussurea lappa
-            (Kur): It is antiseptic & disinfectant. It is very useful in
-            bronchial asthma. Syzygium aromaticum (Labango): Useful in cough &
-            other respiratory diseases. It prevents bad breath. Cinnamomum
-            zeylanicum (Darchini): It is antibacterial & antifungal and relieves
-            cough due to its soothing action. Elettaria cardamomum (Elachi): It
-            is aromatic in nature and shows antimicrobial activity. Cinnamomum
-            tamala (Tejpata): It is a tonic & appetizer agent. Pistacia
-            integerrima (Kakra sringi): It helps in asthma. Myrica nagi (
-            Kotfal): It is a remedy for sore throat. Woodfordia fruticosa
-            (Daiful): It is good anti-infective agent and is useful in relieving
-            cough due to its soothing action. Tulsi extract & other ingredients:
-            Tulsi extract & some other ingredients are added to DEVAS syrup for
-            relieving acute cough.
-            <br />
-            <br />
-            Pregnancy: The safety of this syrup in pregnancy has not been
-            studied. Therefore, it should be used with caution during pregnancy.
-          </p>
-          <Alert className="mt-9">
-            <InfoIcon />
-            <AlertTitle>Disclaimer</AlertTitle>
-            <AlertDescription>
-              The information provided is accurate to our best practices, but it
-              does not replace professional medical advice. We cannot guarantee
-              its completeness or accuracy. The absence of specific information
-              about a drug should not be seen as an endorsement. We are not
-              responsible for any consequences resulting from this information,
-              so consult a healthcare professional for any concerns or
-              questions.
-            </AlertDescription>
-          </Alert>
+          {tab === "overview" ? <OverviewComponent /> : <ReviewComponent />}
         </section>
       </div>
     </>
--- src/components/modules/shopPage/review.tsx
+++ src/components/modules/shopPage/review.tsx
@@ -1,10 +1,45 @@
-export default function ReviewComponent(){
+import PaginationControl from "@/components/shared/pagination";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 
- return (
-  <>
-   <div>
-      <h1>This is  Name page component.</h1>
-   </div>
-  </>
-);
+export default function ReviewComponent() {
+  return (
+    <>
+      <div className="mt-7 flex flex-col gap-7">
+        <h1 className="text-4xl font-semibold">Customer Reviews</h1>
+        <div className="grid lg:grid-cols-2 gap-3">
+          {Array.from({ length: 5 }).map((_, idx) => (
+            <Card className="w-full">
+              <CardHeader>
+                <Badge className="font-semibold">03 Days ago</Badge>
+              </CardHeader>
+              <CardContent className="h-full flex flex-col items-center justify-center text-sm text-muted-foreground">
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi
+                  fugit molestiae sint nostrum alias? Porro ullam distinctio
+                  quae, impedit iusto, provident a excepturi reiciendis minus
+                  quisquam laudantium, nulla quis totam.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <div className="flex flex-row-reverse items-center justify-start gap-3 w-full">
+                  <Avatar className="w-10 h-10">
+                    <AvatarFallback>NS</AvatarFallback>
+                  </Avatar>
+                  <h1 className="text-md font-semibold">Nostrum Store</h1>
+                </div>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+        <PaginationControl currentPage={1} totalPages={5} />
+      </div>
+    </>
+  );
 }
